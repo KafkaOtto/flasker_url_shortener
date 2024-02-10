7425395956665fe00c3b00df0287924f75c4dd29
XitongWU <wuxitong0@gmail.com>
2024-02-10 15:34:26 +0100

    Update url_controller.py

--- app/controller/url_controller.py
+++ app/controller/url_controller.py
@@ -40,29 +40,29 @@ def api_delete_all_urls():
 
 # Not yet in progresss
 
-@api.route('/users/<string:id>', methods=['PUT'])
-def api_put(id):
-    ''' Update entity by id'''
-    body = request.json
-    body['id'] = id
-    res = url_service.put(body)
-    return jsonify(res.as_dict()) if isinstance(res, User) else jsonify(res)
+# @api.route('/users/<string:id>', methods=['PUT'])
+# def api_put(id):
+#     ''' Update entity by id'''
+#     body = request.json
+#     body['id'] = id
+#     res = url_service.put(body)
+#     return jsonify(res.as_dict()) if isinstance(res, User) else jsonify(res)
 
-@api.route('/users/<string:id>', methods=['DELETE'])
-def api_delete(id):
-    ''' Delete entity by id'''
-    res = url_service.delete(id)
-    return jsonify(res)
+# @api.route('/users/<string:id>', methods=['DELETE'])
+# def api_delete(id):
+#     ''' Delete entity by id'''
+#     res = url_service.delete(id)
+#     return jsonify(res)
 
-@api.errorhandler(HTTPException)
-def handle_exception(e):
-    """Return JSON format for HTTP errors."""
-    # start with the correct headers and status code from the error
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        'success': False,
-        "message": e.description
-    })
-    response.content_type = "application/json"
-    return response
+# @api.errorhandler(HTTPException)
+# def handle_exception(e):
+#     """Return JSON format for HTTP errors."""
+#     # start with the correct headers and status code from the error
+#     response = e.get_response()
+#     # replace the body with JSON
+#     response.data = json.dumps({
+#         'success': False,
+#         "message": e.description
+#     })
+#     response.content_type = "application/json"
+#     return response
